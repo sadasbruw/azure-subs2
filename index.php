3296824e1d29839977c0f8b0e3c51616cdcdfd0a
sadasbruw <sadasbruw@gmail.com>
2019-08-02 00:37:01 +0700

    menambah captions

--- index.php
+++ index.php
@@ -32,7 +32,7 @@ $listBlobsOptions->setPrefix("");
     <!-- Required meta tags -->
     <meta charset="utf-8">
     <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
-
+    <script src="http://ajax.googleapis.com/ajax/libs/jquery/1.9.0/jquery.min.js"></script>
     <!-- Bootstrap CSS -->
     <link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.3.1/css/bootstrap.min.css" integrity="sha384-ggOyR0iXCbMQv3Xipma34MD+dH/1fQ784/j6cY/iJTQUOhcWr7x9JvoRxT2MZw1T" crossorigin="anonymous">
   </head>
@@ -75,6 +75,8 @@ $listBlobsOptions->setPrefix("");
         .done(function(data) {
             // Show formatted JSON on webpage.
             $("#responseTextArea").val(JSON.stringify(data, null, 2));
+            // mengambil captions
+            $("#caption").text(data.description.captions[0].text);
         })
  
         .fail(function(jqXHR, textStatus, errorThrown) {
@@ -138,6 +140,8 @@ $listBlobsOptions->setPrefix("");
                       <br><br>
                       <textarea id="responseTextArea" class="UIInput"
                                 style="width:580px; height:400px;"></textarea>
+                      <br>
+                      <h2 id="caption">...</h2>
                   </div>
                   <div id="imageDiv" style="width:420px; display:table-cell;">
                       Source image:
@@ -152,7 +156,6 @@ $listBlobsOptions->setPrefix("");
     <!-- Optional JavaScript -->
     <!-- jQuery first, then Popper.js, then Bootstrap JS -->
     
-    <script src="http://ajax.googleapis.com/ajax/libs/jquery/1.9.0/jquery.min.js"></script>
     <script src="https://cdnjs.cloudflare.com/ajax/libs/popper.js/1.14.7/umd/popper.min.js" integrity="sha384-UO2eT0CpHqdSJQ6hJty5KVphtPhzWj9WO1clHTMGa3JDZwrnQq4sF86dIHNDz0W1" crossorigin="anonymous"></script>
     <script src="https://stackpath.bootstrapcdn.com/bootstrap/4.3.1/js/bootstrap.min.js" integrity="sha384-JjSmVgyd0p3pXB1rRibZUAYoIIy6OrQ6VrjIEaFf/nJGzIxFDsf4x0xIM+B07jRM" crossorigin="anonymous"></script>
   </body>
